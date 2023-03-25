d0ca87c415c0f8649a91adf9b9c973d75eba8a04
joelee90 <dlwhdgur@h-brick.com>
2023-03-25 19:14:26 +0900

    FEAT: initial customizing

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,23 +2,21 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: `JongHyeok's Blog`,
+  author: 'JongHyeok Lee',
+  headerTitle: `_Blog`,
+  description: '안녕하세요, FE 개발자 이종혁입니다.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://joeleeblog.vercel.app/',
+  siteRepo: 'https://github.com/joelee90/mynextblog',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/profile.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'jonghyeoklee90@gmail.com',
+  github: 'https://github.com/joelee90',
+  twitter: 'https://twitter.com/jhl_90',
+  linkedin: 'https://www.linkedin.com/in/jonghyeoklee90/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -65,20 +63,6 @@ const siteMetadata = {
       lang: 'en',
     },
   },
-  // search: {
-  //   provider: 'kbar', // kbar or algolia
-  //   kbarConfig: {
-  //     searchDocumentsPath: 'search.json', // path to load documents to search
-  //   },
-  //   provider: 'algolia',
-  //   algoliaConfig: {
-  //     // The application ID provided by Algolia
-  //     appId: 'R2IYF7ETH7',
-  //     // Public API key: it is safe to commit it
-  //     apiKey: '599cec31baffa4868cae4e79f180729b',
-  //     indexName: 'docsearch',
-  //   },
-  // },
 }
 
 module.exports = siteMetadata
